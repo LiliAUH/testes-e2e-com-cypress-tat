f04c825d9793a3f7ad13055b40f80a39a8bc205a
LiliAUH <aline.hatakeyama@gmail.com>
2024-07-17 10:43:20 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -5,17 +5,15 @@ export default defineConfig({
   chromeWebSecurity: false,
   viewportWidth: 1600,
   viewportHeight: 900,
+  
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     baseUrl: 'https://notes-serverless-app.com',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     env: {
       viewportWidthBreakpoint: 768,
-    },
-    
+    },  
   },
+  projectId: 'pn8fw7',
 })
 
 
